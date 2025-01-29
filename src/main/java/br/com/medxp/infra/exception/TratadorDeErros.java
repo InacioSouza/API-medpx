35a7ba8434eaf8deefa7c31408c9f8fb88e8c7b1
Inácio Souza Rocha <inaciosouzarocha69@gmail.com>
2025-01-29 16:02:38 -0300

    Funcionalidade de segurança implementada (Login)

--- src/main/java/br/com/medxp/infra/exception/TratadorDeErros.java
+++ src/main/java/br/com/medxp/infra/exception/TratadorDeErros.java
@@ -1,6 +1,8 @@
 package br.com.medxp.infra.exception;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -15,9 +17,21 @@ public class TratadorDeErros {
 		return ResponseEntity.notFound().build();
 	}
 
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<?> tratarErro400(MethodArgumentNotValidException ex) {
+		var erros = ex.getFieldErrors();
+		return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList());
+	}
+
 	@ExceptionHandler(ValidacaoException.class)
 	public ResponseEntity<?> tratarErroRegraDeNegócio(ValidacaoException ex) {
 		return ResponseEntity.badRequest().body(ex.getMessage());
+	}
+
+	public record DadosErroValidacao(String campo, String mensagemm) {
+		public DadosErroValidacao(FieldError erro) {
+			this(erro.getField(), erro.getDefaultMessage());
+		}
 	}
 
 }
--- src/main/java/br/com/medxp/infra/security/TokenService.java
+++ src/main/java/br/com/medxp/infra/security/TokenService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 
 import br.com.medxp.domain.usuario.Usuario;
 
@@ -31,6 +32,20 @@ public class TokenService {
 		} catch (JWTCreationException exception) {
 			throw new RuntimeException("Erro ao gerar token JWT", exception);
 		}
+	}
+
+	public String getSubject(String tokenJWT) {
+
+		try {
+			var algoritmo = Algorithm.HMAC256(secret);
+
+			return JWT.require(algoritmo).withIssuer("API medxp").build().verify(tokenJWT).getSubject();
+
+		} catch (JWTVerificationException exception) {
+
+			throw new RuntimeException("Token JWT inválido ou expirado!");
+		}
+
 	}
 
 	private Instant dataExpiracao() {
